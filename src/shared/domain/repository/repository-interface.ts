469aa2d13b7f3d58a8d4365987e9b35954ef32bb
Lucas Rodrigues de Souza <Rodriguesdesouzal28@gmail.com>
2024-10-14 09:06:34 -0300

    feat: interface de repositório para buscar personalizados

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -1,5 +1,7 @@
 import { Entity } from "../entity";
 import { ValueObject } from "../value-object";
+import { SearchParams } from "./search-params";
+import { SearchResult } from "./search-result";
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>;
@@ -11,4 +13,14 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   findAll(): Promise<E[]>;
 
   getEntity(): new (...args: any[]) => E;
+}
+
+export interface ISearchRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+  searchInput = SearchParams,
+  searchOutput = SearchResult
+> extends IRepository<E, EntityId> {
+  sortableFields: string[];
+  search(props: searchInput): Promise<searchOutput>;
 }
