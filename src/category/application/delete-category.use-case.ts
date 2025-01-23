5f34396fd676eae39ed43313f066c614bb3b62cc
lucas <lucas.rodrigues@neocredit.com.br>
2025-01-23 20:20:09 -0300

    test: testes delete categoria

--- src/category/application/delete-category.use-case.ts
+++ src/category/application/delete-category.use-case.ts
@@ -2,7 +2,7 @@ import { IUseCase } from "../../shared/application/use-case.interface";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { ICategoryRepository } from "../domain/category.repository";
 
-export class UpdateCategoryUseCase
+export class DeleteCategoryUseCase
   implements IUseCase<DeleteCategoryInput, DeleteCategoryOutput>
 {
   constructor(private readonly categoryRepo: ICategoryRepository) {}
