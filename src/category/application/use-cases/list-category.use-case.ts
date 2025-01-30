241892034110d75a3c3eccb515bb0b1d874261ad
lucas <lucas.rodrigues@neocredit.com.br>
2025-01-30 19:10:31 -0300

    feat; get all categorias

---
+++ src/category/application/use-cases/list-category.use-case.ts
@@ -0,0 +1,44 @@
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from "../../../shared/application/pagination-output";
+import { IUseCase } from "../../../shared/application/use-case.interface";
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import { Category } from "../../domain/category.entity";
+import {
+  CategoryFilter,
+  CategorySearchParams,
+  CategorySearchResult,
+  ICategoryRepository,
+} from "../../domain/category.repository";
+import { CategoryOutput, CategoryOutputMapper } from "./commom/category-output";
+
+export class ListCategoryUseCase
+  implements IUseCase<ListCategoryInput, ListCategoryOutput>
+{
+  constructor(private readonly categoryRepo: ICategoryRepository) {}
+
+  async execute(input: ListCategoryInput): Promise<ListCategoryOutput> {
+    const params = new CategorySearchParams(input);
+    const searchResult = await this.categoryRepo.search(params);
+    return this.toOutput(searchResult);
+  }
+
+  private toOutput(searchResult: CategorySearchResult): ListCategoryOutput {
+    const { items: _items } = searchResult;
+    const items = _items.map((i) => {
+      return CategoryOutputMapper.toOutput(i);
+    });
+    return PaginationOutputMapper.toOutput(items, searchResult);
+  }
+}
+
+export type ListCategoryInput = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  sort_dir?: SortDirection | null;
+  filter?: CategoryFilter | null;
+};
+
+export type ListCategoryOutput = PaginationOutput<CategoryOutput>;
